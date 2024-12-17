f80d8c2093c556e9a12267c056d12c2b4d6eda54
cornip <62475127+cornip@users.noreply.github.com>
2024-12-18 02:15:09 +0700

    Delete src directory

--- src/characteristics/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod bio;
-pub mod lore;
-pub mod adjectives;
-pub mod post_examples; 
-pub mod styles;
-pub mod topics;
--- src/core/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod agent;
-pub mod characteristics;
-pub mod instruction_builder;
-pub mod runtime;
-pub mod character;
